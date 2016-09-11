25ef0aa9a31b3f9bbd3b80485ce28ed5c72a70a1
silence20141022 <108948450@qq.com>
2016-09-11 14:28:43 +0800

    设置基础脚本和菜单
    设置基础脚本和菜单

---
+++ Controllers/CommonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SceneOfCustoms.Controllers
+{
+    public class CommonController : Controller
+    {
+        //
+        // GET: /Common/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string CurrentUser()
+        {
+            //JObject json_user = Extension.Get_UserInfo(HttpContext.User.Identity.Name);
+            //return json_user.GetValue("REALNAME") + "";
+            return "测试用户";
+        }
+	}
+}
