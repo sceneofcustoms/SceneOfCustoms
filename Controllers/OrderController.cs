45212062f9c4b1e22cb413d0fe159f588e75f331
luohaolakers <luohylakers@foxmail.com>
2016-09-11 18:47:29 +0800

    动态数据
    动态数据

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SceneCustoms.Common;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +16,23 @@ namespace SceneOfCustoms.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public string GetData()
+        {
+
+            var str1 = "{\"total\":\"28\",\"rows\":[{\"productid\":\"FI-SW-01\",\"productname\":\"Koi\",\"unitcost\":\"10.00\",\"status\":\"P\",\"listprice\":\"36.50\",\"attr1\":\"Large\",\"itemid\":\"EST-1\"}]}";
+
+
+            string sql = "select t.*, t.rowid from list_order t where t.busitype='11'";
+            DataTable dt = DBMgr.GetDataTable(sql);
+            string result = JsonConvert.SerializeObject(dt);
+            var str= "{\"total\":\"28\",\"rows\":"+result+"}";
+
+            return str;
+
+
         }
 	}
 }
