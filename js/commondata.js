fd2a20759c2448ed4df29e47d0c6d65ee2e48f0a
silence20141022 <108948450@qq.com>
2016-09-17 16:48:59 +0800

    commondata

--- js/commondata.js
+++ js/commondata.js
@@ -1,1 +1,7 @@
-﻿
+﻿//公共静态数据
+
+//1 查询条件 --日期
+var search_condition_date = [{ code: 'jiedan', name: '接单日期' }, { code: 'lihuo', name: '理货日期' }, { code: 'jihuabaoguan', name: '计划报关日期' },
+    { code: 'lihuoziliaoqiquan', name: '理货资料齐全日期' }, { code: 'baoguan', name: '报关日期' }, { code: 'danzhengfangxing', name: '单证放行日期' },
+    { code: 'shiwufangxing', name: '实物放行日期' }, { code: 'chuguanfeng', name: '出关封日期' }, { code: 'koufangxing', name: '口岸放行时间' },
+    { code: '', name: '口岸安排报关时间' }];
