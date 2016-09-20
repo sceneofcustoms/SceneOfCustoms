7232444ad3e8291a056c14547469860a3fec577d
luohaolakers <luohylakers@foxmail.com>
2016-09-20 18:39:21 +0800

    添加model

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace SceneOfCustoms.Models
+{
+    public class Order
+    {
+
+    }
+}
