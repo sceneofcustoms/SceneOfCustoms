fa33c0dc1f5298a45ab570be3b16973cbd2c7ba6
DLCCLD <494791736@qq.com>
2016-09-22 08:50:00 +0800

    小杜

---
+++ Controllers/RetreatorderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SceneOfCustoms.Controllers
+{
+    public class RetreatorderController : Controller
+    {
+        // 退单管理列表
+        public ActionResult BackOrder_List()
+        {
+            ViewData["crumb"] = "退单管理-->退单管理";
+            return View();
+        }
+        // 签收操作列表
+        public ActionResult SignOrder_List()
+        {
+            ViewData["crumb"] = "退单管理-->签收操作";
+            return View();
+        }
+    }
+}
