f9d8c7eb06e2581ea9a1bd5690dbc99c6d58e6a1
DLCCLD <494791736@qq.com>
2016-10-20 16:22:17 +0800

    导出方法修改

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -396,55 +396,6 @@ namespace SceneOfCustoms.Controllers
             }
             workbook.Save(path);
         }
-        public MemoryStream OutFileToStream(DataTable dt)
-        {
-            Workbook workbook = new Workbook(); //工作簿 
-            Worksheet sheet = workbook.Worksheets[0]; //工作表 
-            Cells cells = sheet.Cells;//单元格  
-            //样式2 
-            Style style2 = workbook.Styles[workbook.Styles.Add()];//新增样式 
-            //style2.HorizontalAlignment = TextAlignmentType.Center;//文字居中 
-            style2.Font.Name = "宋体";//文字字体 
-            style2.Font.Size = 9;//文字大小 
-            style2.Font.IsBold = true;//粗体 
-            //style2.IsTextWrapped = true;//单元格内容自动换行 
-            style2.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
-            style2.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
-            style2.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
-            style2.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
-            //样式3 
-            Style style3 = workbook.Styles[workbook.Styles.Add()];//新增样式 
-            //style3.HorizontalAlignment = TextAlignmentType.Center;//文字居中 
-            style3.Font.Name = "宋体";//文字字体 
-            style3.Font.Size = 9;//文字大小 
-            style3.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
-            style3.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
-            style3.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
-            style3.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
-            int Colnum = dt.Columns.Count;//表格列数 
-            int Rownum = dt.Rows.Count;//表格行数 
-            cells.SetRowHeight(0, 38);
-            //生成行2 列名行 
-            for (int i = 0; i < Colnum; i++)
-            {
-                cells[0, i].PutValue(dt.Columns[i].ColumnName);
-                cells[0, i].SetStyle(style2);
-                cells.SetRowHeight(0, 25);
-            }
-            //生成数据行 
-            for (int i = 0; i < Rownum; i++)
-            {
-                for (int k = 0; k < Colnum; k++)
-                {
-                    cells[1 + i, k].PutValue(dt.Rows[i][k].ToString());
-                    cells[1 + i, k].SetStyle(style3);
-                }
-                cells.SetRowHeight(1 + i, 24);
-            }
-            MemoryStream ms = workbook.SaveToStream();
-            return ms;
-        }
-
         //申报关区 进口口岸 
         public string Get_SBGQ()
         {
